fa472d7dbb054526e873f36089595e65f7fdd5cd
YasserBakkar <yasserbakkar97@gmail.com>
2023-05-20 00:59:00 +0300

    Add Question Model

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -15,7 +16,9 @@ class MainActivity : AppCompatActivity() {
             if(etName!!.text!!.isEmpty()){
                 Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
             }else{
-
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
             }
         }
     }
