2de95f7e8b3484946e3dd1ae8b217fd03dc9b2f9
YasserBakkar <yasserbakkar97@gmail.com>
2023-05-19 23:57:04 +0300

    Add btnStart

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener {
+
+            if(etName!!.text!!.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+
+            }
+        }
     }
 }
