216b499a8d71d77e5ce71692ed33caba462e1a2d
YasserBakkar <yasserbakkar97@gmail.com>
2023-05-22 18:13:57 +0300

    Add Question UI

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,23 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionList = Constants.getQuestions()
         Log.i("QuestionList size is ", "${questionList.size}")
+
+        for(i in questionList){
+            Log.e("Questionss", i.question)
+        }
+
+        var currentPosition = 1
+        val question : Question = questionList[currentPosition-1]
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "${currentPosition}/${progressBar?.max}"
+        tvQuestion?.text = question.question
+        ivFlag?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
+
+
     }
 }
